7ea209b8f08e53b1c53b34e5b1ab0c20d3cb70c3
mikzanco <mikyzancolo97@gmail.com>
2022-12-01 14:46:11 +0100

    add array, bootrstap and table bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +57,43 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Hotel List</h1>
+    </header>
+    <main>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Parcheggio</th>
+            <th scope="col">Stelle</th>
+            <th scope="col">Distanza dal centro</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td >Larry the Bird</td>
+            <td>@twitter</td>
+            <td>@mdo</td>
+            </tr>
+        </tbody>
+    </table>
+    </main>
 </body>
 </html>
